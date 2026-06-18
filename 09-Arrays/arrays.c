74dc97c401920f9e99dfc0df79faef7eb250bd50
Tan5dev <tanmaypathe4@gmail.com>
2026-06-18 20:57:43 +0530

    Create 09-Arrays/arrays.c

---
+++ 09-Arrays/arrays.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int marks[5] = {85, 90, 78, 92, 88};
+
+    printf("First mark: %d\n", marks[0]);
+    printf("Second mark: %d\n", marks[1]);
+
+    return 0;
+}
