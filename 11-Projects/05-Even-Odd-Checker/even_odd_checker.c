f721e5da96554ca84cbe36c252793eba3c8121e6
Tan5dev <tanmaypathe4@gmail.com>
2026-06-19 07:05:58 +0530

    Create 05-Even-Odd-Checker/even_odd_checker.c

---
+++ 11-Projects/05-Even-Odd-Checker/even_odd_checker.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    int number;
+
+    printf("=== Even Odd Checker ===\n\n");
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    if(number % 2 == 0) {
+        printf("%d is an Even Number.\n", number);
+    }
+    else {
+        printf("%d is an Odd Number.\n", number);
+    }
+
+    return 0;
+}
